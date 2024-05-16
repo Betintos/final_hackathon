f5c97b6504e8b6e82ee05830ce309c086ef77edd
AdiletCH <adimaster2008@gmail.com>
2024-05-16 17:58:56 +0600

    бот

--- config/bot.py
+++ config/bot.py
@@ -1,10 +1,16 @@
-from .settings import TELEGRAM_TOKEN
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import telebot
+from django_filters import FilterSet
+from settings import TELEGRAM_TOKEN
+from telegram.ext import Updater, CommandHandler, MessageHandler
+
 
 TOKEN = TELEGRAM_TOKEN
+bot = telebot.TeleBot(TOKEN)
 
-def start(update, context):
-    update.message.reply_text('Привет, чем могу помочь?')
+@bot.message_handler(commands=['start'])
+def start(message):
+    message2 = bot.send_message(message.chat.id, 'Привет, чем могу помочь?')
+    bot.register_next_step_handler(message2)
 
 def echo(update, context):
     update.message.reply_text(update.message.text)
@@ -13,7 +19,7 @@ def main():
     updater = Updater(TELEGRAM_TOKEN, use_context=True)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dp.add_handler(MessageHandler(FilterSet.text & ~FilterSet.command, echo))
 
 
     updater.start_polling()
@@ -23,3 +29,4 @@ if __name__ == '__main__':
     main()
 
 
+bot.polling(none_stop=True)
